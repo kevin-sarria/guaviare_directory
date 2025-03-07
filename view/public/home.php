c14a64f39b225941faf7e9104155cab5648a6e87
GuelYend <miguelortiz.c25@gmail.com>
2025-03-07 13:15:07 -0500

    Las imagenes del encabezado en (view/public/home) ahora rotan automaticamente

--- view/public/home.php
+++ view/public/home.php
@@ -1,5 +1,5 @@
 <section>
-    <div id="carouselExample" class="carousel slide position-relative">
+<div id="carouselExample" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
         <div class="carousel-inner">
             <div class="carousel-item active w-100" style="height: 40vh; width: 100%;">
                 <img src="https://media.istockphoto.com/id/1317323736/photo/a-view-up-into-the-trees-direction-sky.jpg?s=612x612&w=0&k=20&c=i4HYO7xhao7CkGy7Zc_8XSNX_iqG0vAwNsrH1ERmw2Q=" class="w-100 h-100 object-fit-cover" alt="...">
